9ae2c759ad5994ab94a11e9df550ff7defa9f24c
ayush.sharma <ayush.sharm@olx.com>
2023-06-25 20:41:37 +0530

    add main class

--- src/main/java/com/jobcoinmixer/app/JobcoinMixerApplication.java
+++ src/main/java/com/jobcoinmixer/app/JobcoinMixerApplication.java
@@ -1,13 +1,20 @@
 package com.jobcoinmixer.app;
 
+import com.jobcoinmixer.app.config.ApplicationProperties;
+import com.jobcoinmixer.app.config.SeedJobcoinsInHouseAccountConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@EnableConfigurationProperties(ApplicationProperties.class)
+@Import(SeedJobcoinsInHouseAccountConfig.class)
 public class JobcoinMixerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JobcoinMixerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobcoinMixerApplication.class, args);
+    }
 
 }
